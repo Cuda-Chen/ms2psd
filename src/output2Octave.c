51c12599f49c48138ab083401ce76c4caa550ff2
Cuda-Chen <clh960524@gmail.com>
2021-05-06 16:38:08 +0800

    add Octave script output

---
+++ src/output2Octave.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "output2Octave.h"
+
+void
+output2Octave (const char outputfile, int nfft, float *psd)
+{
+  FILE *fid = fopen (outputfile, "w");
+  fprintf (fid, "%% %s : auto-generated file\n", outputfile);
+  fprintf (fid, "clear all;\n");
+  fprintf (fid, "close all;\n\n");
+  fprintf (fid, "nfft = %u;\n", nfft);
+  fprintf (fid, "f    = [0:(nfft-1)]/nfft - 0.5;\n");
+  fprintf (fid, "psd  = zeros(1,nfft);\n");
+
+  for (i = 0; i < nfft; i++)
+    fprintf (fid, "psd(%6u) = %12.4e;\n", i + 1, psd[i]);
+
+  fprintf (fid, "figure;\n");
+  fprintf (fid, "plot(f, psd, '-', 'LineWidth',1.5);\n");
+  fprintf (fid, "xlabel('Normalized Frequency [f/F_s]');\n");
+  fprintf (fid, "ylabel('Power Spectral Density [dB]');\n");
+  fprintf (fid, "grid on;\n");
+  fprintf (fid, "axis([-0.5 0.5 ymin ymax]);\n");
+
+  fclose (fid);
+}
---
+++ src/output2Octave.h
@@ -0,0 +1,6 @@
+#ifndef OUTPUT2OCTAVE_H
+#define OUTPUT2OCTAVE_H
+
+void output2Octave (const char *outputfile, int nfft, float *psd);
+
+#endif
