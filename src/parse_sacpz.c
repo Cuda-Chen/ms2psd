7a13c352136d7e9bd32743e78136654c95b40568
Cuda-Chen <clh960524@gmail.com>
2021-06-18 15:29:58 +0800

    parse test of SACPZ

--- src/parse_sacpz.c
+++ src/parse_sacpz.c
@@ -1,6 +1,7 @@
 #include <complex.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define LEN 0x200
 
@@ -16,10 +17,33 @@ parse_sacpz (const char *sacpzfile, double complex **poles,
   }
 
   char buffer[LEN];
+  int read_zeros = 0, read_poles = 0;
+  int nzeros, npoles;
 
   while (fgets (buffer, LEN, fp))
   {
-    printf ("%s\n", buffer);
+    char *pch = strtok(buffer, " ");
+    if(strcmp(pch, "CONSTANT") == 0) {
+        printf("CONSTANT ");
+        printf("%s", pch);
+        pch = strtok(NULL, " ");
+        printf(" %s", pch);
+    } else if(strcmp(pch, "ZEROS") == 0) {
+        printf("ZEROS");
+        printf("%s", pch);
+        pch = strtok(NULL, " ");
+        printf(" %s", pch);
+    } else if(strcmp(pch, "POLES") == 0) {
+        printf("POLES");
+        printf("%s", pch);
+        pch = strtok(NULL, " ");
+        printf(" %s", pch);
+    } else {
+        printf("%s", pch);
+        pch = strtok(NULL, " ");
+        printf(" %s", pch);
+    }
+    printf("\n");
   }
 
   fclose (fp);
