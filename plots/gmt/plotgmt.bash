76559df75f8d8e3a4e6ba96e1bfc9b3ecf69c5b3
Cuda Chen <clh960524@gmail.com>
2021-09-05 20:36:59 +0800

    plot grid with no data when using GMT
    Plot grid with no data when using GMT.
    Seems that converted netCDF grid contains no data.

--- plots/gmt/plotgmt.bash
+++ plots/gmt/plotgmt.bash
@@ -4,10 +4,14 @@ PDFDATA=../../pdf_out.txt
 PDFPERIODS=../../center_periods_out.txt
 HIGHNOISEMODEL=../../highnoise.mod
 LOWNOISEMODEL=../../lownoise.mod
+COLORMAP=psdpdf.cpt
 
 PDFGRID=pdf.grd
 PLOTFILE=plot.ps
 
-gmt xyz2grd ${PDFDATA} -R0/102/0/150 -I1/1 -G${PDFGRID}
-
-gmt grdview ${PDFGRID} -JX6i/5i -Ba1.0:"log10(Period)":/a10f5:"Power [10log10(m**2/sec**4/Hz)] [dB]":Wsne -pdf testplot
+gmt begin example
+    gmt xyz2grd ${PDFDATA} -R0/102/0/150 -I1/1 -G${PDFGRID} -V
+    gmt grdview ${PDFGRID} -JX6i/5i -Bxa1.0+l"log10(Period)" -Bya10f5+l"Power [10log10(m**2/sec**4/Hz)] [dB]" -BWsne -C${COLORMAP} -S100 -Qs -N0 -V -Y4.0
+    gmt colorbar -C${COLORMAP} -B.02 -D6.15i/2.5i/5.0i/0.25i -V
+    #gmt psconvert ${PLOTFILE} -A -Tf -V
+gmt end show
