4e8e241dc178acc2287a6c4dd13cf57a7a2c0863
Cuda Chen <clh960524@gmail.com>
2021-09-05 23:20:28 +0800

    create first desired GMT plot
    Create first desired GMT plot.
    Need further tuning including commands and input data format.

--- plots/gmt/plotgmt.bash
+++ plots/gmt/plotgmt.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -eux
+
 PDFDATA=../../pdf_out.txt
 PDFPERIODS=../../center_periods_out.txt
 HIGHNOISEMODEL=../../highnoise.mod
@@ -10,7 +12,7 @@ PDFGRID=pdf.grd
 PLOTFILE=plot.ps
 
 gmt begin example
-    gmt xyz2grd ${PDFDATA} -R0/102/0/150 -I1/1 -G${PDFGRID} -V
+    gmt xyz2grd ${PDFDATA} -R0/102/0/150 -I1/1 -G${PDFGRID} -ZTLA -V
     gmt grdview ${PDFGRID} -JX6i/5i -Bxa1.0+l"log10(Period)" -Bya10f5+l"Power [10log10(m**2/sec**4/Hz)] [dB]" -BWsne -C${COLORMAP} -S100 -Qs -N0 -V -Y4.0
     gmt colorbar -C${COLORMAP} -B.02 -D6.15i/2.5i/5.0i/0.25i -V
     #gmt psconvert ${PLOTFILE} -A -Tf -V
